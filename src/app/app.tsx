08822676239a3171172e2d230942ad463678d999
Dmitry Makhnev <dmitriy.makhnev@jugru.org>
2020-01-30 01:55:01 +0300

    feat(prettier): base setting up, connection with eslint;

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './app.scss';
 
 export const App: React.FC = () => (
-  <div className={styles.App}>
+  <div className={styles.App} lang="ru">
     Hello
     <div className={styles.planetIcon} />
   </div>
